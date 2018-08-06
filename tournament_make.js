bb9068182c4642c104c6fc46dccbd4c38ad54db5
flubbateios <flubbateios@gmail.com>
2018-08-06 12:47:20 +0100

    minor fix for tournaments

--- tournament_make.js
+++ tournament_make.js
@@ -44,7 +44,7 @@ var model = new (function () {
 				that.minute(that.getUTCMinutes());
 			}
 		});
-		self.urlData.time && that.date(self.urlData.time);
+		self.urlData.date && that.date(self.urlData.date);
 	})();
 	self.live = ko.observable(self.urlData.live === undefined ? false : self.urlData.live);
 	self.canSubmit = ko.computed(function () {
